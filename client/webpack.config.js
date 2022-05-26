ebcf9dbb79cefbe02e5376c5c26e4e2d72c5571c
rich p <richardferry3@gmail.com>
2022-05-26 07:07:26 -0700

    add notes to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
 
 module.exports = () => {
   return {
@@ -18,6 +17,7 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      //configure workbox plugins for a service worker and manifest file
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin',
@@ -47,6 +47,7 @@ module.exports = () => {
 
     module: {
       rules: [
+        //CSS loaders and babel to webpack.
         {
           test: /\.css$/i,
           use: ["style-loader", "css-loader"],
