307bb15dd2034887d315f734c4da1bd2661ae1ea
rich p <richardferry3@gmail.com>
2022-05-26 05:25:38 -0700

    add InjectManifest as plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,6 +22,11 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),
+      
       
     ],
 
