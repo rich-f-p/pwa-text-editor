401821c21e113a50eb62b9e63e07592cb8da6fb5
rich p <richardferry3@gmail.com>
2022-05-26 19:04:39 -0700

    add put and get logic so data is consistent with online and offline versions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('putDb update');
+  const jateDb = await openDB('jate', 1);
+  const text = jateDb.transaction('jate', 'readwrite');
+  const store = text.objectStore('jate');
+  const request = store.put({jate: content});
+  const results = await request;
+  console.log('update: ', results);
+}
+// logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('grabbing data');
+  const jateDb = await openDB('jate', 1);
+  const text = jateDb.transaction('jate', 'readonly');
+  const store = text.objectStore('jate');
+  const request = store.getAll();
+  const results = await request;
+  console.log('results: ', results);
+};
 
 initdb();
